a2f638b3d70f7d417be68cb12cc2e38d82f8fa0e
mahmoud45rashad <113264517+mahmoud45rashad@users.noreply.github.com>
2024-05-01 18:51:40 +0300

    Update Navs.js

--- src/component/Navs/Navs.js
+++ src/component/Navs/Navs.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar';
+import { Image } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import logo from '../../assets/logo.png'
 import '../Navs/Navs.css';
 const Navs = () => {
@@ -18,22 +22,22 @@ const Navs = () => {
   //   </Container>
   // </Navbar>
 
-<nav className='navbar navbar-expand-lg navbar-light sticky-top'>
-<div className='container'>
-<a className='navbar-brand' href="#home"><img src={logo}/></a>
+<Navbar expand="lg"  sticky="top">
+<Container>
+<Navbar.Brand href="#home"><Image src={logo}/></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <div className='navbar-collapse collapse' id="basic-navbar-nav">
-    <div className='nav'>
-      <a className='nav-link' href="#home">Home</a>
-      <a  className='nav-link' href="#link">About</a>
-      <a className='nav-link' href="#link">Explore Food</a>
-      <a className='nav-link' href="#link">Review</a>
-      <a  className='nav-link' href="#link">Faq</a>
-      <a  className='nav-link link-call' href="#deets" >1800 789 123</a>
-    </div>
-  </div>
-</div>
-</nav>
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav>
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">About</Nav.Link>
+      <Nav.Link href="#link">Explore Food</Nav.Link>
+      <Nav.Link href="#link">Review</Nav.Link>
+      <Nav.Link href="#link">Faq</Nav.Link>
+      <Nav.Link href="#deets" className='link-call'>1800 789 123</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Container>
+</Navbar>
   )
 }
 
